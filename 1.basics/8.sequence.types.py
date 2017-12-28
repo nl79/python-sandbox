ae35e8a1c6a76acfda8a1198645f2c7fadc36a2b
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2017-12-27 20:53:03 -0500

    update: sequences and iterators

--- 1.basics/8.sequence.types.py
+++ 1.basics/8.sequence.types.py
@@ -1,24 +1,38 @@
 # ipAddress = input('Please enter an IP address')
 # print(ipAddress.count('.'))
 
-parrot_list = ['non pinin', 'no more', 'a stiff', 'bareft of life']
-parrot_list.append('A norwegian blue')
+# parrot_list = ['non pinin', 'no more', 'a stiff', 'bareft of life']
+# parrot_list.append('A norwegian blue')
+#
+# for state in parrot_list:
+#     print('This parrot is {}'.format(state))
+#
+# even = [2,4,6,8]
+# odd = [1,3,5,7,9]
+#
+# number = even + odd
+# number.sort()
+# print(number)
+#
+# #return a new object
+# sortedNumbers = sorted(number)
+# print(sortedNumbers)
+#
+# if number == sortedNumbers:
+#     print('The lists are equal')
+# else:
+#     print('The list are not equal')
 
-for state in parrot_list:
-    print('This parrot is {}'.format(state))
+menu = []
+menu.append(['egg', 'spam', 'bacon'])
+menu.append(['egg', 'sausage', 'baconn'])
+menu.append(['egg', 'spam', 'bascon'])
+menu.append(['egg', 'bacon', 'sausage', 'spam'])
+menu.append(['spam', 'egg', 'spam', 'bacon', 'spam'])
 
-even = [2,4,6,8]
-odd = [1,3,5,7,9]
+print(menu)
 
-number = even + odd
-number.sort()
-print(number)
-
-#return a new object
-sortedNumbers = sorted(number)
-print(sortedNumbers)
-
-if number == sortedNumbers:
-    print('The lists are equal')
-else:
-    print('The list are not equal')
+for meal in menu:
+    if not 'spam' in meal:
+        for item in meal:
+            print(item)
---
+++ 1.basics/9.iterator.py
@@ -0,0 +1,53 @@
+string = '1234567890'
+
+# for char in string:
+#     print(char)
+# for char in iter(string):
+#     print(char)
+# my_iterator = iter(string)
+# print(my_iterator)
+# print(next(my_iterator))
+# print(next(my_iterator))
+
+# print(range(100))
+# my_list = list(range(10))
+# print(my_list)
+# even = list(range(0,10,2))
+# odd = list(range(1, 10, 2))
+# print(even)
+# print(odd)
+
+# my_string = 'abcdefghijklmnopqrstuvwxyz'
+# print(my_string.index('a'))
+# print(my_string[4])
+#
+# small_decimals = range(0, 10)
+# print(small_decimals)
+#
+# print(small_decimals.index(3))
+#
+# odd = range(1, 10000, 2)
+# print(odd)
+# print(odd[984])
+#
+# sevens = range(7, 10000, 7)
+# print(sevens)
+#
+# print(range(0,5,2) == range(0,6,2)) #true
+#
+# r = range(0, 100)
+# print(r)
+#
+# for i in r[::-2]:
+#     print(i)
+#
+# for i in range(99, 2, -2):
+#     print(i)
+#
+# print('=' * 50)
+#
+# print(range(0, 100)[::-2] == range(99, 0, -2))
+#
+# for i in range(0, 100, -2):
+#     print(i)
+
