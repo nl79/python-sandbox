6dc06e425338b0906a0e0559bc4e5b180468c0c2
Nazar <nl79@njit.edu>
2017-12-28 08:17:15 -0500

    Sequences

--- 1.basics/8.sequence.types.py
+++ 1.basics/8.sequence.types.py
@@ -1,24 +1,49 @@
 # ipAddress = input('Please enter an IP address')
 # print(ipAddress.count('.'))
 
-parrot_list = ['non pinin', 'no more', 'a stiff', 'bareft of life']
-parrot_list.append('A norwegian blue')
-
-for state in parrot_list:
-    print('This parrot is {}'.format(state))
+# parrot_list = ['non pinin', 'no more', 'a stiff', 'bareft of life']
+# parrot_list.append('A norwegian blue')
+#
+# for state in parrot_list:
+#     print('This parrot is {}'.format(state))
+#
+# even = [2,4,6,8]
+# odd = [1,3,5,7,9]
+#
+# number = even + odd
+# number.sort()
+# print(number)
+#
+# #return a new object
+# sortedNumbers = sorted(number)
+# print(sortedNumbers)
+#
+# if number == sortedNumbers:
+#     print('The lists are equal')
+# else:
+#     print('The list are not equal')
+#
+# list_1 = []
+# list_2 = list()
+#
+# print("List 1: {}".format(list_1))
+# print("List 2: {}".format(list_2))
+# if(list_1 == list_2):
+#     print('Lists are equal')
 
 even = [2,4,6,8]
-odd = [1,3,5,7,9]
+odd = [1, 3, 5, 7, 9]
+# another_even = even
+# another_even.sort(reverse=True)
+# print(even)
+# print(even is another_even)
+# another = list(even)
+# print(even is another)
 
-number = even + odd
-number.sort()
-print(number)
+number = [even, odd]
 
-#return a new object
-sortedNumbers = sorted(number)
-print(sortedNumbers)
+for nums in number:
+    print(nums)
 
-if number == sortedNumbers:
-    print('The lists are equal')
-else:
-    print('The list are not equal')
+    for val in nums:
+        print(val)
