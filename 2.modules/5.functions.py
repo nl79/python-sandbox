8f935b9a0abde6fe5b7a3ec422242802e3563714
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2018-01-15 20:53:30 -0500

    functions

--- 2.modules/5.functions.py
+++ 2.modules/5.functions.py
@@ -6,13 +6,18 @@ def python_foo():
     print(" " * left_margin, text)
 
 
-def center_text(text):
-    text = str(text)
+def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    return " " * left_margin + text
 
-#Call the function
-center_text('spam and eggs')
-center_text('spam, spam and eggs')
-center_text('spam, spam, spam and spam')
-center_text(12)
+
+# Call the function
+s1 = center_text('spam and eggs')
+s2 = center_text('spam, spam and eggs')
+s3 = center_text('spam, spam, spam and spam')
+s4 = center_text(12)
+s5 = center_text('first', 'second', 3, 8, 'spam')
+print(s5)
