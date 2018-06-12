32b9477ed2c3be31dbd7ca6c9079fcd14ad488af
Nazar Lesiv <nazarlesiv@naz-mbpr-15-2015.home>
2018-06-11 21:20:23 -0400

    initial sobel filter example

---
+++ image-processing/sobel/sobel.py
@@ -0,0 +1,12 @@
+# ref: https://blog.saush.com/2011/04/20/edge-detection-with-the-sobel-operator-in-ruby/
+kernelX = [
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1]
+]
+
+kernelY = [
+    [-1, -2, -1],
+    [0, 0, 0],
+    [1, 2, 1]
+]
